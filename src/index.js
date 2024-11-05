e7901fa989a349576bf9b618dd0cac86b700611a
Dario <grba.work@gmail.com>
2024-11-05 15:56:19 +0100

    Initial commit with TypeScript project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function greet(name) {
+    return `Hello, ${name}! Welcome to TypeScript.`;
+}
+const user = 'Dario';
+console.log(greet(user));
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(name: string): string {
+  return `Hello, ${name}! Welcome to TypeScript.`
+}
+
+const user = 'Dario'
+console.log(greet(user))
