661862f2983c40e24b1a7e0585c95cd1439d8633
Dario <grba.work@gmail.com>
2024-11-06 21:52:09 +0100

    start over

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function greet(name) {
-    return `Hello, ${name}! Welcome to TypeScript.`;
-}
-const user = 'Dario';
-console.log(greet(user));
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-function greet(name: string): string {
-  return `Hello, ${name}! Welcome to TypeScript.`
-}
-
-const user = 'Dario'
-console.log(greet(user))
